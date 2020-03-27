9c89c18ca58e6b100e82975f9ffc9bfef6d33061
Emilio <emiliogzz24@gmail.com>
2020-03-27 00:31:29 -0600

    Uploading lab

---
+++ index.js
@@ -0,0 +1,26 @@
+const list = document.querySelector('#list');
+
+const addItem = (ev) => {
+    ev.preventDefault()
+    var input = form.querySelector('input').value;
+    if (input != '') {
+        var el = document.createElement('li');
+        el.setAttribute('class', 'list-item')
+        var domString = `<p>${input}</p><div class="buttons"><button>check</button><button>delete</button></div>`;
+        el.addEventListener("click", e => {
+            if(e.target) { 
+                if (e.target.innerHTML == "delete") {
+                    e.target.parentElement.parentNode.remove();
+                } else if (e.target.innerHTML == "check") {
+                    e.target.parentElement.parentElement.querySelector('p').style.textDecoration = "line-through";
+                }
+            }
+        })
+        el.innerHTML = domString;
+        list.prepend(el)
+        form.querySelector('input').value = ''
+    }
+}
+
+const form = document.querySelector('#form');
+form.addEventListener('submit', addItem);
